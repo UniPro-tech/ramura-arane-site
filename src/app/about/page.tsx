adcd2c55f24ee5f37b327edb9f4c9831e1c0a2ec
YuitoAkatsuki <yuito_it@outlook.jp>
2024-08-08 18:41:19 +0900

    :bug: 適用ミス

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -53,39 +53,46 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-[#1f1f1f] text-white bg-[url('../images/wipe.png')]">
-      <div id="about-desc" className="m-9 p-9">
-        <h1 className="text-6xl font-bold">About</h1>
-        <ul className="text-1xl leading-6 p-4">
-          <li>
-            名前:
-            <ruby>
-              現音令彼<rt>あらねしむか</rt>
-            </ruby>
-          </li>
-          <li>英語表記: AraneShimuka</li>
-          <li>性別: 不明</li>
-          <li>声別: 女性</li>
-          <li>種族: 人外</li>
-          <li>年齢: ？(少なくとも人間を超えてる)</li>
-          <li>身長: 173.5(変更可)</li>
-          <li>誕生日: 7月8日</li>
-          <li>持ち物: ドライフラワー</li>
-          <li>趣味: 猫吸い</li>
-          <li>好きなもの: 猫(特に長毛種)</li>
-          <li>苦手なもの: 湿度と花粉(花粉症)</li>
-          <li>一人称: 私</li>
-          <li>二人称: 君、あなた</li>
-          <li>三人称: 君たち、あなた達</li>
-        </ul>
-      </div>
-      <Image
-        className="leading-none m-0 p-0"
-        src="/images/fullbody.png"
-        alt="logo"
-        width={500}
-        height={0}
+    <>
+      <script
+        key="json-ld"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
-    </main>
+      <main className="flex min-h-screen items-center justify-center bg-[#1f1f1f] text-white bg-[url('../images/wipe.png')]">
+        <div id="about-desc" className="m-9 p-9">
+          <h1 className="text-6xl font-bold">About</h1>
+          <ul className="text-1xl leading-6 p-4">
+            <li>
+              名前:
+              <ruby>
+                現音令彼<rt>あらねしむか</rt>
+              </ruby>
+            </li>
+            <li>英語表記: AraneShimuka</li>
+            <li>性別: 不明</li>
+            <li>声別: 女性</li>
+            <li>種族: 人外</li>
+            <li>年齢: ？(少なくとも人間を超えてる)</li>
+            <li>身長: 173.5(変更可)</li>
+            <li>誕生日: 7月8日</li>
+            <li>持ち物: ドライフラワー</li>
+            <li>趣味: 猫吸い</li>
+            <li>好きなもの: 猫(特に長毛種)</li>
+            <li>苦手なもの: 湿度と花粉(花粉症)</li>
+            <li>一人称: 私</li>
+            <li>二人称: 君、あなた</li>
+            <li>三人称: 君たち、あなた達</li>
+          </ul>
+        </div>
+        <Image
+          className="leading-none m-0 p-0"
+          src="/images/fullbody.png"
+          alt="logo"
+          width={500}
+          height={0}
+        />
+      </main>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,32 @@ import Image from "next/image";
 import { Button } from "@/components/button";
 import type { Metadata } from "next";
 
+import {
+  Article as JsonLDArticle,
+  WebPage as JsonLDWebPage,
+  WithContext,
+} from 'schema-dts';
+
+const jsonLd: WithContext<JsonLDArticle | JsonLDWebPage> = {
+  '@context': 'https://schema.org',
+  '@type': 'WebPage',
+  name: 'About - 荒音の夜 - UTAU配布サイト',
+  description: '現音令彼(あらねしむか)の公式設定についての情報です。使用する前にご一読ください。',
+  url: 'https://arane.uniproject-tech.net/about',
+  inLanguage: 'ja',
+  image: {
+    '@type': 'ImageObject',
+    url: 'https://arane.uniproject-tech.net/images/title.png',
+    license: "https://arane.uniproject-tech.net/license",
+    creditText: "ramura 2024",
+    creator: {
+      '@type': "Person",
+      name: "ramura",
+    },
+    copyrightNotice: "ramura"
+  },
+};
+
 export const metadata: Metadata = {
   title: "荒音の夜 - 現音令彼(あらねしむか)公式サイト",
   description:
@@ -25,25 +51,32 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-0 m-0 justify-center items-center bg-[#1f1f1f] bg-[url('../images/wipe.png')]">
-      <div id="description" className="flex flex-col m-9 p-9 leading-none">
-        <h1 className="text-7xl font-bold text-white m-0 font-BIZMincho leading-none">
-          <ruby>
-            現音令彼<rt>あらねしむか</rt>
-          </ruby>
-        </h1>
-        <Button<"Link"> href="/about">About</Button>
-        <Button<"Link"> href="/download">Download</Button>
-        <Button<"Link"> href="/license">License</Button>
-        <Button<"Link"> href="/contact">Contacts</Button>
-      </div>
-      <Image
-        className="leading-none m-0 p-0"
-        src="/images/fullbody.png"
-        alt="logo"
-        width={500}
-        height={0}
+    <>
+      <script
+        key="json-ld"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
-    </main>
+      <main className="flex min-h-screen p-0 m-0 justify-center items-center bg-[#1f1f1f] bg-[url('../images/wipe.png')]">
+        <div id="description" className="flex flex-col m-9 p-9 leading-none">
+          <h1 className="text-7xl font-bold text-white m-0 font-BIZMincho leading-none">
+            <ruby>
+              現音令彼<rt>あらねしむか</rt>
+            </ruby>
+          </h1>
+          <Button<"Link"> href="/about">About</Button>
+          <Button<"Link"> href="/download">Download</Button>
+          <Button<"Link"> href="/license">License</Button>
+          <Button<"Link"> href="/contact">Contacts</Button>
+        </div>
+        <Image
+          className="leading-none m-0 p-0"
+          src="/images/fullbody.png"
+          alt="logo"
+          width={500}
+          height={0}
+        />
+      </main>
+    </>
   );
 }
