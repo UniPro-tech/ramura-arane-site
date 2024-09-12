850ab5297a7456a6e661477d77df78f6a5af1141
唯人(Yuito Akatsuki) <yuito@yuito.work>
2024-09-12 09:05:42 +0900

    Add special sound source (#37)
    * :sparkles: 特殊音源の追加
    
    * :sparkles: 特殊音源についての説明を追加

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -29,26 +29,40 @@ export default function Home() {
     <main className="flex min-h-screen p-0 m-0 justify-center items-center bg-[#1f1f1f] bg-[url('../images/wipe.png')]">
       <div>
         <h1 className="text-4xl text-white font-bold">Download</h1>
-        <p className="text-white">ダウンロードする前に、一度、利用規約をお読みください。</p>
+        <p className="text-white">
+          ダウンロードする前に、一度、利用規約をお読みください。<br />
+          なお、特殊音源に関しては、単体では使用できません。特殊音素のエイリアスはEです。
+        </p>
         <br />
-        <Button<"Link">
-          href="/download/dl-endpoint?file=tandokuon.zip"
-          className="mt-4"
-        >
-          単独音(4音階)
-        </Button>
-        <Button<"Link">
-          href="/download/dl-endpoint?file=renzokuon.zip"
-          className="mt-4"
-        >
-          連続音(1音階)
-        </Button>
-        <Button<"Link">
-          href="/download/dl-endpoint?file=tougou.zip"
-          className="mt-4"
-        >
-          統合音源(連続1音階/単独音4音階)
-        </Button>
+        <div className="flex">
+          <Button<"Link">
+            href="/download/dl-endpoint?file=tandokuon.zip"
+            className="mt-4"
+          >
+            単独音(4音階)
+          </Button>
+          <Button<"Link">
+            href="/download/dl-endpoint?file=renzokuon.zip"
+            className="mt-4"
+          >
+            連続音(1音階)
+          </Button>
+        </div>
+        <div className="flex">
+          <Button<"Link">
+            href="/download/dl-endpoint?file=tougou.zip"
+            className="mt-4"
+          >
+            統合音源(連続1音階/単独音4音階)
+          </Button>
+          <Button<"Link">
+            href="/download/dl-endpoint?file=tokushu.zip"
+            className="mt-4"
+          >
+            特殊音源
+          </Button>
+        </div>
+
       </div>
       <Image
         className="leading-none m-0 p-0"
